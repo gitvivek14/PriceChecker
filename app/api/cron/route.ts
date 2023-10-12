a368d70ae4deb0793a1fc81af6fef8021ef43d3d
gitvivek <varora2004@gmail.com>
2023-10-13 02:05:25 +0530

    fix-errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,11 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+
+export const maxDuration = 300;
+export const dynamic = 'force-dynamic'
+export const revalidate = 0;
+
 export async function GET() {
 try {
     connecttoDb();
@@ -31,7 +36,7 @@ try {
     
 
 const updatedProduct = await Product.findOneAndUpdate(
-    {url:scrapedProduct.url} ,
+    {url:product.url} ,
      product,
     ) ;
 
